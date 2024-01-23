ffbdb9efabcb681e047776a3fd2c05c25ae354a9
Emmanuel James <114751376+Eastsydgod@users.noreply.github.com>
2024-01-23 02:55:02 -0500

    Add files via upload
    Home Page Interface

---
+++ site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+// Done By: Emmanuel James
+// Last Modified: 2024/01/23
+// Write your JavaScript code.
+var player = new Vimeo.Player(document.getElementById('videoFrame'));
+
+player.setLoop(true);
+player.setVolume(0);
+player.play();
