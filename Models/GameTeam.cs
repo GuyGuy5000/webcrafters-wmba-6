a5032458238ffb49540a167516a5da8aaec8c28b
GuyGuy5000 <51391711+GuyGuy5000@users.noreply.github.com>
2024-01-25 13:04:46 -0500

    Finalized models folder

---
+++ Models/GameTeam.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace wmbaApp.Models
+{
+    /// <summary>
+    /// A class for individual games that teams are assigned to.
+    /// - Nadav Hilu
+    /// </summary>
+    public class GameTeam
+    {
+        [Display(Name = "Team")]
+        public int TeamID { get; set; }
+        public Team Team { get; set; }
+
+        [Display(Name = "Game")]
+        public int GameID { get; set; }
+        public Game Game { get; set; }
+
+        [Required(ErrorMessage = "A lineup is required")]
+        [Display(Name = "Lineup")]
+        [StringLength(2000)]
+        public string GmtmLineup { get; set; }
+        [Display(Name = "Score")]
+        public int? GmtmScore { get; set; }
+    }
+}
