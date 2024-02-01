8141da04b5e6b4c4c598b1fd5f8b03936b650c07
farooq106 <34919198+farooq106@users.noreply.github.com>
2024-02-01 12:09:08 -0500

    Updated Player.cs

--- wmbaAppP1/wmbaApp/wmbaApp/Models/Player.cs
+++ wmbaAppP1/wmbaApp/wmbaApp/Models/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using wmbaApp.ViewModels;
 
 namespace wmbaApp.Models
@@ -7,7 +7,7 @@ namespace wmbaApp.Models
     /// A class for players in a team
     /// - Nadav Hilu, Emmanuel James
     /// </summary>
-    public class Player : IValidatableObject
+    public class Player
     {
         #region Summaries
         [Display(Name = "Player Info")]
@@ -40,9 +40,6 @@ namespace wmbaApp.Models
         [Display(Name = "Jersey Number")]
         [Range(1,99, ErrorMessage = "Jeersey numbers are between 1 and 99")]
         public int? PlyrJerseyNumber { get; set; }
-        [Display(Name = "Date of Birth")]
-        [DisplayFormat(DataFormatString = "{0:d MMMM yyyy}")]
-        public DateTime? PlyrDOB { get; set; }
 
         [Display(Name = "Team")]
         public int TeamID { get; set; }
@@ -51,30 +48,8 @@ namespace wmbaApp.Models
         public int? StatisticID { get; set; }
         public Statistic Statistics { get; set; }
 
-        [Display(Name = "Player Photo")]
-        public PlayerPhoto PlayerPhoto { get; set; }
-        public PlayerThumbnail PlayerThumbnail { get; set; }
-
         [Display(Name = "Player Positions")]
         public ICollection<PlayerPosition> PlayerPositions { get; set; } = new HashSet<PlayerPosition>();
-
-        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-        {
-            if (PlyrDOB >= DateTime.Now)
-            {
-                yield return new ValidationResult("Date of birth cannot be in the future", new[] { "PlyrDOB" });
-            }
-
-            int minAge = 1;
-            int maxAge = 40;
-            DateTime minDateOfBirth = DateTime.Now.AddYears(-maxAge);
-            DateTime maxDateOfBirth = DateTime.Now.AddYears(-minAge);
-
-            if (PlyrDOB < minDateOfBirth || PlyrDOB > maxDateOfBirth)
-            {
-                yield return new ValidationResult($"Date of birth must be between {minAge} and {maxAge} years of old", new[] { "PlyrDOB" });
-            }
-        }
 
     }
 }
