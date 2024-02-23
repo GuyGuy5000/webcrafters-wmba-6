39fc39ec1c60bd0d74193db4f4c56389eaaf9219
GuyGuy5000 <nadavhilu12@gmail.com>
2024-02-23 16:09:09 -0500

    attempt to commit Emmanuel's solution

--- ScoreKeeping.cs
+++ ScoreKeeping.cs
@@ -6,14 +6,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+<<<<<<< HEAD
 
 namespace wmbaApp.Models
+=======
+using wmbaApp.Models;
+
+namespace wmbaApp.ViewModels
+>>>>>>> dc85b26 (Add files via upload)
 {
     public class ScoreKeeping
     {
         // Fields or Properties
         private int AtBats;
+<<<<<<< HEAD
         private int[] Hits ;
+=======
+        private int[] Hits;
+>>>>>>> dc85b26 (Add files via upload)
         private int Doubles;
         private int Triples;
         private int HomeRuns;
@@ -22,6 +32,7 @@ namespace wmbaApp.Models
         private int HitByPitches;
         private int Sacrifices;
         private int CatcherInterference;
+<<<<<<< HEAD
 
         // Construvtor that instantiates the fields.
         public ScoreKeeping(int atBats, int [] hits, int doubles, int triples, int homeRuns, int plateAppearances, 
@@ -37,6 +48,40 @@ namespace wmbaApp.Models
             this.HitByPitches = hitsByPitches;
             this.Sacrifices = sacrifices;
             this.CatcherInterference = catcherInterference;
+=======
+        private int Balls;
+        private int Strikes;
+        private int Fouls;
+        private int Runs;
+        private int Outs;
+
+
+        private bool FirstBase { get; set; }
+        private bool SecondBase { get; set; }
+        private bool ThirdBase { get; set; }
+        private bool BasesLoaded => FirstBase && SecondBase && ThirdBase;
+
+        // Constructor that instantiates the fields.
+        public ScoreKeeping(int atBats, int[] hits, int doubles, int triples, int homeRuns, int plateAppearances,
+            int walks, int hitsByPitches, int sacrifices, int catcherInterference, int ballsCount, int strikesCount,
+            int foulsCount, int runsCount, int outsCount)
+        {
+            AtBats = atBats;
+            Hits = hits;
+            Doubles = doubles;
+            Triples = triples;
+            HomeRuns = homeRuns;
+            PlateAppearances = plateAppearances;
+            Walks = walks;
+            HitByPitches = hitsByPitches;
+            Sacrifices = sacrifices;
+            CatcherInterference = catcherInterference;
+            Runs = runsCount;
+            Fouls = foulsCount;
+            Balls = ballsCount;
+            Outs = outsCount;
+            Strikes = strikesCount;
+>>>>>>> dc85b26 (Add files via upload)
         }
 
         // Properties to determine the type of hit. PS: Not so sure about this implementation, It can be modified.
@@ -58,7 +103,11 @@ namespace wmbaApp.Models
         // I don't know how we are gonna evaluate a Home-Run. This method evaluates a Hom-Run as four hits by the player.
         public int TotalHomeRuns()
         {
+<<<<<<< HEAD
             return Hits.Count(h => h == 4); 
+=======
+            return Hits.Count(h => h == 4);
+>>>>>>> dc85b26 (Add files via upload)
         }
 
 
@@ -82,7 +131,11 @@ namespace wmbaApp.Models
         }
 
         // Calculates OPS
+<<<<<<< HEAD
         public double CalculateOBP() 
+=======
+        public double CalculateOBP()
+>>>>>>> dc85b26 (Add files via upload)
         {
             double OPS = (double)(TotalSingles() + TotalDoubles() * 2 + TotalTriples() * 3 + TotalHomeRuns() * 4) / AtBats;
             return Math.Round(OPS, 3);
@@ -117,6 +170,239 @@ namespace wmbaApp.Models
             return Math.Round(result, 3);
         }
 
+<<<<<<< HEAD
+=======
+        // PLAYER ACTIONS
+
+
+
+
+
+
+        public enum AtBatOutcome
+        {
+            Ball,
+            CalledStrike,
+            SwingAndMiss,
+            FoulBall,
+            FoulTipOut,
+            HitByPitch,
+            IntentionalBall,
+            IntentionalWalk,
+            CatcherInterference,
+            IllegalPitch,
+            Walk,
+            Strikeout,
+            Hit,
+        }
+
+
+
+        private void RecordHit()
+        {
+            if (!FirstBase)
+            {
+                FirstBase = true;
+            }
+            else if (!SecondBase)
+            {
+                SecondBase = true;
+            }
+            else if (!ThirdBase)
+            {
+                ThirdBase = true;
+            }
+
+        }
+
+
+
+        public void RecordBall() => Balls++;
+
+        public void RecordFoul() => Fouls++;
+        public void AwardRun() => Runs++;
+        private void RecordOut()
+        {
+            Outs++;
+            ResetCount();
+        }
+
+        private void ResetCount()
+        {
+            Balls = 0;
+            Strikes = 0;
+        }
+        public void RecordStrike()
+        {
+            Strikes++;
+            if (Strikes >= 3)
+            {
+                RecordOut();
+            }
+        }
+
+        private void RecordFoulTipOut()
+        {
+            RecordOut();
+        }
+
+        private void RecordHitByPitch()
+        {
+
+            HitByPitches++;
+
+            if (IsBasesLoaded())
+            {
+                AwardRun();
+            }
+
+        }
+
+        private bool IsBasesLoaded()
+        {
+            return BasesLoaded;
+        }
+
+        private void RecordIntentionalBall()
+        {
+
+            RecordBall();
+        }
+
+        private void RecordWalk()
+        {
+           
+            Walks++;
+
+
+        }
+
+        private void RecordCatcherInterference()
+        {
+            CatcherInterference++;
+        }
+
+        private void RecordSwingAndMiss()
+        {
+            Strikes++;
+
+            if (Strikes >= 3)
+            {
+                RecordOut();
+            }
+        }
+
+
+        private void RecordIntentionalWalk()
+        {
+            ResetCount();
+        }
+
+
+
+        private void RecordIllegalPitch()
+        {
+            RecordBall();
+        }
+
+        public AtBatOutcome AnalyzeAtBat()
+        {
+            if (Balls >= 4)
+            {
+                return AtBatOutcome.Walk;
+            }
+            else if (Strikes >= 3)
+            {
+                return AtBatOutcome.Strikeout;
+            }
+            else
+            {
+
+///////Still need to add Logic for fouls
+                return AtBatOutcome.Hit;
+            }
+        }
+
+        public void AnalyzePlayerActions(List<AtBatOutcome> playerActions)
+        {
+            foreach (var action in playerActions)
+            {
+                switch (action)
+                {
+                    case AtBatOutcome.Hit:
+                        RecordHit();
+                        break;
+                    case AtBatOutcome.Ball:
+                        RecordBall();
+                        break;
+                    case AtBatOutcome.CalledStrike:
+                        RecordStrike();
+                        break;
+                    case AtBatOutcome.FoulBall:
+                        RecordFoul();
+                        break;
+                    case AtBatOutcome.SwingAndMiss:
+                        RecordSwingAndMiss();
+                        break;
+
+                    case AtBatOutcome.FoulTipOut:
+                        RecordFoulTipOut();
+                        break;
+                    case AtBatOutcome.HitByPitch:
+                        RecordHitByPitch();
+                        break;
+                    case AtBatOutcome.IntentionalBall:
+                        RecordIntentionalBall();
+                        break;
+                    case AtBatOutcome.IntentionalWalk:
+                        RecordIntentionalWalk();
+                        break;
+                    case AtBatOutcome.CatcherInterference:
+                        RecordCatcherInterference();
+                        break;
+                    case AtBatOutcome.IllegalPitch:
+                        RecordIllegalPitch();
+                        break;
+                }
+            }
+        }
+        public string Name { get; set; }
+        public List<AtBatOutcome> AtBatActions { get; set; } = new List<AtBatOutcome>();
+
+
+        public ScoreKeeping(string name)
+        {
+            Name = name;
+            AtBatActions = new List<AtBatOutcome>();
+
+        }
+
+        public void AddAction(AtBatOutcome action)
+        {
+            AtBatActions.Add(action);
+
+        }
+
+
+        public static List<ScoreKeeping> GetPlayers(Player[] players)
+        {
+            List<ScoreKeeping> displayPlayers = new List<ScoreKeeping>();
+
+            foreach (Player player in players)
+            {
+
+                string playerName = $"{player.PlyrFirstName} {player.PlyrLastName}";
+
+
+                ScoreKeeping playerActions = new ScoreKeeping(playerName);
+
+
+                displayPlayers.Add(playerActions);
+            }
+
+            return displayPlayers;
+        }
+>>>>>>> dc85b26 (Add files via upload)
 
     }
 }
