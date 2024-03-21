e2ed0e3b2fb4f46644f09a8dd74c05c98b90fd15
Farooq <34919198+farooq106@users.noreply.github.com>
2024-03-21 17:55:54 -0400

    Add files via upload

---
+++ wmbaAppP2/wmba/wmbaApp/Data/ApplicationDbInitializer.cs
@@ -0,0 +1,175 @@
+﻿using wmbaApp.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+
+namespace wmbaApp.Data
+{
+        public static class ApplicationDbInitializer
+        {
+            public static async void Seed(IApplicationBuilder applicationBuilder)
+            {
+                ApplicationDbContext context = applicationBuilder.ApplicationServices.CreateScope()
+                    .ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                try
+                {
+                    //Creating database and apply migration if database does not exist
+                    context.Database.Migrate();
+
+                    //Creating Roles
+                    var RoleManager = applicationBuilder.ApplicationServices.CreateScope()
+                        .ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                    string[] roleNames = { "Admin", "Coach", "ScoreKeeper", "Convenor" };
+
+                    IdentityResult roleResult;
+                    foreach (var roleName in roleNames)
+                    {
+                        var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                        if (!roleExist)
+                        {
+                            roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                        }
+                    }
+
+                    //Creating Users
+                    var userManager = applicationBuilder.ApplicationServices.CreateScope()
+                        .ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                    //User:Admin
+                    if (userManager.FindByEmailAsync("admin@outlook.com").Result == null)
+                    {
+                        IdentityUser user = new IdentityUser
+                        {
+                            UserName = "admin@outlook.com",
+                            Email = "admin@outlook.com",
+                            EmailConfirmed = true
+                        };
+
+                        IdentityResult result = userManager.CreateAsync(user, "Wmba@team6").Result;
+
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRoleAsync(user, "Admin").Wait();
+                        }
+                    }
+
+                    //User:Coach
+                    if (userManager.FindByEmailAsync("coach@outlook.com").Result == null)
+                    {
+                        IdentityUser user = new IdentityUser
+                        {
+                            UserName = "coach@outlook.com",
+                            Email = "coach@outlook.com",
+                            EmailConfirmed = true
+                        };
+
+                        IdentityResult result = userManager.CreateAsync(user, "Wmba@team6").Result;
+
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRoleAsync(user, "Coach").Wait();
+                        }
+                    }
+
+                    //User:ScoreKeeper
+                    if (userManager.FindByEmailAsync("scorekeeper@outlook.com").Result == null)
+                    {
+                        IdentityUser user = new IdentityUser
+                        {
+                            UserName = "scorekeeper@outlook.com",
+                            Email = "scorekeeper@outlook.com",
+                            EmailConfirmed = true
+                        };
+
+                        IdentityResult result = userManager.CreateAsync(user, "Wmba@team6").Result;
+
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRoleAsync(user, "ScoreKeeper").Wait();
+                        }
+                    }
+
+                    //User:Convenor
+                    if (userManager.FindByEmailAsync("convenor@outlook.com").Result == null)
+                    {
+                        IdentityUser user = new IdentityUser
+                        {
+                            UserName = "convenor@outlook.com",
+                            Email = "convenor@outlook.com",
+                            EmailConfirmed = true
+                        };
+
+                        IdentityResult result = userManager.CreateAsync(user, "Wmba@team6").Result;
+
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRoleAsync(user, "Convenor").Wait();
+                        }
+                    }
+
+                    //User: Prof.David Stovell
+                    if (userManager.FindByEmailAsync("dstovell@niagaracollege.ca").Result == null)
+                    {
+                        IdentityUser user = new IdentityUser
+                        {
+                            UserName = "dstovell@niagaracollege.ca",
+                            Email = "dstovell@niagaracollege.ca",
+                            EmailConfirmed = true
+                        };
+
+                        IdentityResult result = userManager.CreateAsync(user, "Wmba@team6").Result;
+
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRoleAsync(user, "Admin").Wait();
+                        }
+                    }
+
+                    //User: Prof.Mark Hardwick
+                    if (userManager.FindByEmailAsync("mhardwick@niagaracollege.ca").Result == null)
+                    {
+                        IdentityUser user = new IdentityUser
+                        {
+                            UserName = "mhardwick@niagaracollege.ca",
+                            Email = "mhardwick@niagaracollege.ca",
+                            EmailConfirmed = true
+                        };
+
+                        IdentityResult result = userManager.CreateAsync(user, "Wmba@team6").Result;
+
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRoleAsync(user, "Admin").Wait();
+                        }
+                    }
+
+                    //User: Prof Dave Kendall
+                    if (userManager.FindByEmailAsync("dkendell@niagaracollege.ca").Result == null)
+                    {
+                        IdentityUser user = new IdentityUser
+                        {
+                            UserName = "dkendell@niagaracollege.ca",
+                            Email = "dkendell@niagaracollege.ca",
+                            EmailConfirmed = true
+                        };
+
+                        IdentityResult result = userManager.CreateAsync(user, "Wmba@team6").Result;
+
+                        if (result.Succeeded)
+                        {
+                            userManager.AddToRoleAsync(user, "Admin").Wait();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.GetBaseException().Message);
+                }
+            }
+        }
+
+    }
+
+
