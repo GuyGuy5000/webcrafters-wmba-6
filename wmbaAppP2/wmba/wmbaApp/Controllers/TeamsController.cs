992c8d5fa8ce5f2060b3c0de8a686a7960fbbf22
Farooq <34919198+farooq106@users.noreply.github.com>
2024-03-21 17:41:26 -0400

    Update TeamsController.cs

--- wmbaAppP2/wmba/wmbaApp/Controllers/TeamsController.cs
+++ wmbaAppP2/wmba/wmbaApp/Controllers/TeamsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ using wmbaApp.Utilities;
 
 namespace wmbaApp.Controllers
 {
+[Authorize]
     public class TeamsController : ElephantController
     {
         private readonly WmbaContext _context;
