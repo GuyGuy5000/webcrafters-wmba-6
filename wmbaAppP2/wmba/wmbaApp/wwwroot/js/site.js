f0fb620830ca2961d4df5a9a7817540914b001d1
GuyGuy5000 <nadavhilu12@gmail.com>
2024-03-23 14:53:29 -0400

    added toggle for scorekeeping nav bar

--- wmbaAppP2/wmba/wmbaApp/wwwroot/js/site.js
+++ wmbaAppP2/wmba/wmbaApp/wwwroot/js/site.js
@@ -247,9 +247,11 @@ function showMenu() {
     if (window.location.href.includes("ScoreKeeping")) {
 
         document.getElementById("scoreBar").style.display = "block";
+        document.getElementById("phoneNav").style.display = "none";
     } else {
         // Hides the navigation bar on all other pages
         document.getElementById("scoreBar").style.display = "none";
+        document.getElementById("phoneNav").style.display = "block";
     }
 }
 
