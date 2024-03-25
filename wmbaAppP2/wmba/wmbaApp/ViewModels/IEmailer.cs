8626c8545ab581d8f27fcb7c6b05914dd45e4d28
Farooq <34919198+farooq106@users.noreply.github.com>
2024-03-24 20:07:00 -0400

    Add files via upload

---
+++ wmbaAppP2/wmba/wmbaApp/ViewModels/IEmailer.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Mail;
+
+namespace wmbaApp.ViewModels
+{
+    /// <summary>
+    /// Interface for my own email service
+    /// </summary>
+    public interface IEmailer
+    {
+        Task SendOneAsync(string name, string email, string subject, string htmlMessage);
+        Task SendToManyAsync(EmailMessage emailMessage);
+    }
+}
