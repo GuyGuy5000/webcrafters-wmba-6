36cf824c58cb61aa35cd721ca795572bc244a452
Farooq <34919198+farooq106@users.noreply.github.com>
2024-03-27 23:42:06 -0400

    Delete wmba directory

--- wmba/ViewModels/EmailMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace wmbaApp.ViewModels
-{
-    public class EmailMessage
-    {
-        public List<EmailAddress> ToAddresses { get; set; } = new List<EmailAddress>();
-        public string Subject { get; set; }
-        public string Content { get; set; }
-    }
-}
--- wmba/ViewModels/GameIndexVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace wmbaApp.ViewModels
-{
-    public class GameIndexVM
-    {
-        public string FullVersus { get; set; }
-        public string StartTimeSummary { get; set; }
-
-    }
-}
