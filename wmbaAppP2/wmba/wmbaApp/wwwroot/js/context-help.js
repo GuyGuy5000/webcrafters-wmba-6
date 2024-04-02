de761e11f33e867d936ea5cf1f94cf9d47a8648b
GuyGuy5000 <nadavhilu12@gmail.com>
2024-04-01 22:53:50 -0400

    added context help for UserRole views, ImportTeams view, and homepage

--- wmbaAppP2/wmba/wmbaApp/wwwroot/js/context-help.js
+++ wmbaAppP2/wmba/wmbaApp/wwwroot/js/context-help.js
@@ -2,11 +2,12 @@
 //onload used to ensure all html was loaded before checking if GenerateContextHelp exists in the page
 window.onload = () => {
     RemoveUnusedHelpIcon();
+
     function RemoveUnusedHelpIcon() {
         body = document.body;
         //if GenerateContextHelp was not called, it means the help box is empty and won't display
-        if (!(body.innerHTML.includes('GenerateContextHelp(["'))) {
-            helpIcon = document.getElementsByClassName("help-icon"); //gets the help icon
+        if (!(body.innerHTML.includes('GenerateContextHelp(['))) {
+            helpIcon = document.getElementsByClassName("help-icon-container"); //gets the help icon
             helpIcon[0].remove(); //removes help icon
         }
     }
@@ -22,7 +23,7 @@ function GenerateContextHelp(explanations) {
     let pageNum = document.getElementById("pageNum");
     let helpContent = document.querySelector('.help-content');
     let explanationIndex = 0;
-    //firstTimeHelp();
+    firstTimeHelp();
 
 
     helpIcon.addEventListener('keydown', (event) => {
@@ -66,7 +67,7 @@ function GenerateContextHelp(explanations) {
         function sleep(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
         }
-        await sleep(2000); //waits 2 seconds (2000 miliseconds)
+        await sleep(1000); //waits 2 seconds (2000 miliseconds)
         //check to see if it's a first time visit to the site (will reset when the session ends)
         if (sessionStorage["firstTimeHelpFlag"] == undefined) {
             sessionStorage.setItem("firstTimeHelpFlag", "false"); //set flag to indicate the homepage was visited
